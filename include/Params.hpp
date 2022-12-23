567f246de03d2b3211a9e6e80be0ebe5e5413d2d
SaKaneko <satoshi.kaneko.p8@dc.tohoku.ac.jp>
2022-12-23 20:04:38 +0900

    syuusei initial_endcost

--- include/Params.hpp
+++ include/Params.hpp
@@ -12,7 +12,7 @@ double alpha                           = 1.0;
 double p                               = 0.50;
 const std::vector<double> alpha_loader = {0.25, 0.5, 1.0, 2.0};
 const std::vector<int> nodes_loader    = {20, 40, 60, 80, 100};
-const std::vector<double> p_loader     = {0.25, 0.50, 0.75, 1.00};
+const std::vector<double> p_loader     = {0.25, 0.50, 0.75};
 const int iteration                    = 25;
 enum output_type { Table, Adjacency };
 
